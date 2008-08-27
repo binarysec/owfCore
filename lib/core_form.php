ea4b4859762515e89c96829f7c4f936de2efb83c
keo <info@binarysec.com>
2008-08-27 12:02:54 +0000

    forms en form
    git-svn-id: svn+ssh://svn.owf.re/home/svn/nat/core/branches/1_0@14 f8094afe-d156-4993-a692-4f49031433ae

--- lib/core_form.php
+++ lib/core_form.php
@@ -1,434 +1,67 @@
 <?php
- 
-/* (c) mykii#@! */
 
-define("WF_FORM_TEXT",     100);
-define("WF_FORM_INPUT",    101);
-define("WF_FORM_PASS",     102);
-define("WF_FORM_SELECT",   103);
-define("WF_FORM_HIDDEN",   104);
-define("WF_FORM_CHECKBOX", 105);
-define("WF_FORM_FILE",     106);
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Web Framework 1                                       *
+ * BinarySEC (c) (2000-2008) / www.binarysec.com         *
+ * Author: Michael Vergoz <mv@binarysec.com>             *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~         *
+ *  Avertissement : ce logiciel est protégé par la       *
+ *  loi du copyright et par les traités internationaux.  *
+ *  Toute personne ne respectant pas ces dispositions    *
+ *  se rendra coupable du délit de contrefaçon et sera   *
+ *  passible des sanctions pénales prévues par la loi.   *
+ *  Il est notamment strictement interdit de décompiler, *
+ *  désassembler ce logiciel ou de procèder à des        *
+ *  opération de "reverse engineering".                  *
+ *                                                       *
+ *  Warning : this software product is protected by      *
+ *  copyright law and international copyright treaties   *
+ *  as well as other intellectual property laws and      *
+ *  treaties. Is is strictly forbidden to reverse        *
+ *  engineer, decompile or disassemble this software     *
+ *  product.                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 class core_form {
-	var $wf;
-	var $el = array();
-	var $errors = array();
-	var $p_link = NULL;
-	var $title = NULL;
-	var $b_submit = NULL;
-	var $b_reset = NULL;
-	
-	var $is_multipart = FALSE;
 
-	public function __construct($wf) {
+	private $wf       = null;
+	private $attribs  = array();
+	private $elements = array();
+
+
+	// Constructor
+
+	public function __construct($wf, $id) {
 		$this->wf = $wf;
-	}
-	
-	public function load($link, $title=NULL, $submit=NULL, $reset=NULL) {
-		$this->p_link = $link;
-		$this->title = $title;
-		$this->b_submit = $submit;
-		$this->b_reset = $reset;
+		$this->id = $id;
 	}
 
-	var $division = 0;
-	var $printable_data = 0;
 
-	public function add_text(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_TEXT, $text, $default, $value, $size));
+	// Properties
+
+	public function __set($name, $value) {
+		$this->attribs[$name] = $value;
 	}
 
-	public function add_input(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_INPUT, $text, $default, $value, $size));
+	public function __get($name) {
+		if(array_key_exists($name, $this->attribs))
+			return($this->attribs[$name]);
+		return(null);
 	}
 
-	public function add_pass(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_PASS, $text, $default, $value, $size));
+
+	public function add_element($element) {
+		$this->elements[$element->id] = $element;
 	}
 
-	public function add_select(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_SELECT, $text, $default, $value, $size));
+
+	// Rendering
+
+	public function render($tpl_name) {
+		$tpl = new core_tpl($this->wf);
+		$tpl->set_vars($this->attribs);
+		$tpl->set('elements', $this->elements);
+		return($tpl->fetch($tpl_name));
 	}
 
-	public function add_hidden(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_HIDDEN, $text, $default, $value, $size));
-	}
-
-	public function add_checkbox(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_CHECKBOX, $text, $default, $value, $size));
-	}
-
-	public function add_file(
-		$key,
-		$text,
-		$default=NULL,
-		$value=NULL,
-		$size=30
-		) {
-		return($this->add_element($key, WF_FORM_FILE, $text, $default, $value, $size));
-	}
-	
-	public function add_element(
-			$key,
-			$type,
-			$text,
-			$default=NULL,
-			$value=NULL,
-			$size=30
-		) {
-	
-		if($type != WF_FORM_HIDDEN)
-			$this->printable_data++;
-		
-		if($type == WF_FORM_FILE)
-			$this->is_multipart = TRUE;
-			
-		$insert = array(
-			"K" => $key,
-			"TYPE" => $type,
-			"TEXT" => $text
-		);
-		if($value != NULL)
-			$insert["VAL"] = $value;
-		if($default != NULL)
-			$insert["DFT"] = $default;
-		if($size != NULL)
-			$insert["SIZE"] = $size;
-			
-		$this->el[$key] = $insert;
-		
-		return(TRUE);
-	}
-	
-	public function load_vars() {
-		
-		$ret = array();
-		reset($this->el);
-		foreach($this->el as $k => $v) {
-			$ret[$k] = $this->load_var($k);
-			
-			/* si l'input est de type select on 
-			   vérifie les elements :) */
-			if($v["TYPE"] == WF_FORM_SELECT) {
-				if(!isset($v["VAL"][$ret[$k]])) {
-// 					return(array());
-				}
-				
-			}
-		}
-		
-		return($ret);
-	}
-	
-	public function load_var($name) {
-		global $_GET;
-		global $_POST;
-		if(isset($_GET[$name]))
-			return($_GET[$name]);
-		else if(isset($_POST[$name]))
-			return($_POST[$name]);
-		return(NULL);
-	}
-	
-	public function add_error($str) {
-		array_push($this->errors, $str);
-	}
-	
-	public function get_error() {
-		return($this->errors);
-	}
-	
-	public function set_division($div) {
-		$this->division = $div;
-	}
-	
-	var $no_form = FALSE;
-	public function no_form() {
-		$this->no_form = TRUE;
-	}
-	
-	public function renderer() {
-
-		$tab = new core_form_table($this->wf);
-		
-		/* construit le header */
-		$hdr = NULL;
-		if($this->title != NULL) {
-			$hdr = '<div class="interact_form_header">'.
-				$this->title.
-				' :</div>';
-			
-		}
-		reset($this->errors);
-		foreach($this->errors as $v) {
-			$hdr .= '<div class="interact_form_error">* '.
-				htmlspecialchars($v).
-				'</div>';
-		}
-		$tab->set_header($hdr);
-		
-		/* vérification si multipart est nécessaire */
-		if($this->is_multipart)
-			$multipart = 'enctype="multipart/form-data"';
-			
-		/* construit le rendu */
-		if(!$this->no_form)
-			$tab_r = '<form '.$multipart.' method="post" action="'.$this->p_link.'">';
-			
-		$tab_r .= '<div class="interact_form">';
-		
-		$init = TRUE;
-		reset($this->el);
-		$index = 0;
-		$tab->push_row();
-		foreach($this->el as $k => $v) {
-			$text = $v["TEXT"];
-
-			/* check la division */
-			if($this->division > 1) {
-				if($this->printable_data/$this->division > $index)
-					$tab->push_row();
-				else
-					$index = 0;
-			}
-			else
-				$tab->push_row();
-				
-			$index++;
-			
-			$post_val = $this->load_var($k);
-			if($post_val != FALSE && strlen($v["DFT"]) <= 0)
-				$v["DFT"] = $post_val;
-			$v["DFT"] = htmlspecialchars($v["DFT"]);
-			
-			switch($v["TYPE"]) {
-				case WF_FORM_TEXT:
-					
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					
-					$text = $v["DFT"];
-				
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_LEFT
-					);
-					break;
-					
-				case WF_FORM_INPUT:
-					if($v["SIZE"] != NULL) {
-						$size = ' size="'.
-							$v["SIZE"].
-							'"';
-					}
-					
-					$i = '<input '.
-						$size.
-						' type="text" name="'.
-						$k.
-						'" value="'.
-						$v["DFT"].
-						'">'."\n";
-					
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					$tab->add_field(
-						$i,
-						WF_TAB_HALIGN_LEFT
-					);
-					break;
-					
-				case WF_FORM_PASS:
-					if($v["SIZE"] != NULL) {
-						$size = ' size="'.
-							$v["SIZE"].
-							'"';
-					}
-					
-					$i = '<input '.
-						$size.
-						' type="password" name="'.
-						$k.
-						'">'."\n";
-					
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					$tab->add_field(
-						$i,
-						WF_TAB_HALIGN_LEFT
-					);
-					
-					break;
-				
-				case WF_FORM_SELECT:
-					$i = '<select '.
-						' name="'.$k.'">'."\n";
-					reset($v["VAL"]);
-					foreach($v["VAL"] as $vk => $vv) {
-						if($vk == $v["DFT"])
-							$selected = " selected";
-						else 
-							$selected = NULL;
-						
-						$i .= '<option value="'.
-							$vk.
-							'"'.
-							$selected.
-							'>'.
-							$vv.
-							"</option>\n";
-						
-						
-					}
-					$i .= "</select>\n";
-
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					$tab->add_field(
-						$i,
-						WF_TAB_HALIGN_LEFT
-					);
-					break;
-					
-				case WF_FORM_HIDDEN:
-					$i = '<input type="hidden" name="'.
-						$k.
-						'" value="'.
-						$v["DFT"].
-						'">'."\n";
-					
-					$tab_r .= $i;
-					break;
-
-				case WF_FORM_CHECKBOX:
-					if($v["DFT"] == TRUE)
-						$checked = " checked";
-					else
-						$checked = NULL;
-					
-					$i = '<input type="checkbox" name="'.
-						$k.
-						'" value="'.
-						$v["VAL"].
-						'"'.
-						$checked
-						.'>'."\n";
-					
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					$tab->add_field(
-						$i,
-						WF_TAB_HALIGN_LEFT
-					);
-					
-					break;
-
-				case WF_FORM_FILE:
-					if($v["SIZE"] != NULL) {
-						$size = ' size="'.
-							$v["SIZE"].
-							'"';
-					}
-					
-					$i = '<input '.
-						$size.
-						' type="file" name="'.
-						$k.
-						'" value="'.
-						$v["DFT"].
-						'">'."\n";
-					
-					$tab->add_field(
-						$text,
-						WF_TAB_HALIGN_RIGHT,
-						FALSE
-					);
-					$tab->add_field(
-						$i,
-						WF_TAB_HALIGN_LEFT
-					);
-					break;
-					
-			}
-		
-		}
-		
-		if(!$this->no_form) {
-			$buf = '<div class="interact_form_footer">';
-			if($this->b_submit) {
-				$b_submit = ' value="'.$this->b_submit.'"';
-				$buf .= '<input class="core_form_submit" type="submit"'.$b_submit.'>';
-			}
-			if($this->b_reset) {
-				$b_reset = ' value="'.$this->b_reset.'"';
-				$buf .= '<input class="core_form_submit" type="reset"'.$b_reset.'> ';
-			}
-			$buf .= '</div>';
-		}
-		
-		$tab->set_footer($buf);
-		
-		$tab_r .= $tab->renderer();
-		$tab_r .= "</div>\n";
-		
-		if(!$this->no_form)
-			$tab_r .= "</form>\n";
-		
-		return($tab_r);
-	}
-	
 }
-
-?>
