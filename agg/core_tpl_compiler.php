07178ddd78b3f9cdb8df9b70cf86db4dd048ec88
keo <info@binarysec.com>
2009-02-02 14:59:38 +0000

    modif dans main.php pour checker les FILES

--- agg/core_tpl_compiler.php
+++ agg/core_tpl_compiler.php
@@ -518,6 +518,9 @@ class core_tpl_compiler extends wf_agg {
 
 	/* alternating text */
 	public function func_alt(core_tpl_compiler $tpl_compiler, $argv) {
+		if($argv[1]) {
+			return('$alt = !$alt; echo ($alt) ? '.$argv[0].' : '.$argv[1].';');
+		}
 		return('$alt = !$alt; echo ($alt) ? '.$argv[0].' : \'\';');
 	}
 
