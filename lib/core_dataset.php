937d70da7fd9530feab88473195b881387e97602
keo <info@binarysec.com>
2009-03-17 17:57:32 +0000

    dataset qui rox

--- lib/core_dataset.php
+++ lib/core_dataset.php
@@ -22,95 +22,118 @@
  *  product.                                             *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/*
-TODO: regarder les slides 
-filtre:
-	- nom du champs de la zone = array()
-		- traduction SELECT / SLIDE / ACTIVATE
-		
-		- si SELECT 
-			- description = texte
-			- traduction = array()
-				nom en base = traduction
-			
-		- si ACTIVATE
-			- description = texte
-			- on = contrainte base
-			- off = contrainte base
-		
-		
-order = array()
-	liste des champs de la zone 
-
-vue des champs = array()
-	- nom du champs de la zone
-		- description
-		% callback par ligne
-		
-champs de control = array()
-	- array()
-		- nom du head
-		- défini si le champs est libre (graphiquement)
-		- type CALLBACK / TEXT
-		
-		- si CALLBACK = callback
-		- si TEXT = texte à afficher
-		
-*/
-
-define('WF_CORE_DATASET_SELECT_FILTER',   1);
-define('WF_CORE_DATASET_SLIDE_FILTER',    2);
-define('WF_CORE_DATASET_ACTIVATE_FILTER', 3);
+define('WF_CORE_DATASET_SELECT',   1);
+define('WF_CORE_DATASET_SLIDE',    2);
+define('WF_CORE_DATASET_ACTIVATE', 3);
 
 class core_dataset {
 
-	private $wf      = null;
-	private $dsrc    = null;
-	private $filters = array();
+	private $fw   = null;
+	private $dsrc = null;
+
+	private $cols    = array();
+	private $conds   = array();
 	private $order   = array();
-	private $fields  = array();
-	private $fields_callback = array();
-	private $form_filter     = array();
+	private $filters = array();
+	private $row_callback   = null;
+	private $rows_per_page  = null;
+	private $page_nb        = 1;
 
 	public function __construct($wf, $dsrc) {
-		$this->wf  = $wf;
+		$this->wf   = $wf;
 		$this->dsrc = $dsrc;
+
+		/* convert datasource struct to columns format */
+		$struct = $this->dsrc->get_struct();
+		foreach($struct as $name => $type) {
+			$this->cols[$name] = array(
+				'name' => $name
+			);
+		}
 	}
 
-	/* set filters */
-	public function set_filters($filters) {
-		$this->filters = $filters;
+	public function auto_order() {
+		$order = $this->wf->get_var($this->dsrc->get_name().'_order');
+		if(!is_array($order) || !$order) {
+			return;
+		}
+		$orders = array();
+		foreach($order as $col => $way) {
+			if($way) {
+				$this->set_order(array($col => ($way == 'D') ? WF_DESC : WF_ASC));
+			}
+		}
+	}
 
-		/* apply conditions */
-		$this->form_filter = $this->wf->get_var('filter');
-		$conds = array();
+	public function set_order($order) {
+		$this->order = array();
+		$struct = $this->dsrc->get_struct();
+		foreach($order as $col => $way) {
+			if($struct[$col] && $this->cols[$col]['orderable'] == true) {
+				$this->order[$col] = $way;
+			}
+		}
+	}
+
+	public function set_conds($conds) {
+		$pconds = array();
 		foreach($this->filters as $col => $conf) {
-			if($this->form_filter[$col]) {
-				if($conf['type'] == WF_CORE_DATASET_SELECT_FILTER) {
-					$conds[] = array($col, '==', $this->form_filter[$col]);
+			if($conds[$col]) {
+				if($conf['type'] == WF_CORE_DATASET_SELECT) {
+					$pconds[] = array($col, '~=', $conds[$col]);
 				}
 			}
 		}
-		$this->dsrc->apply_conds($conds);
+		$this->conds = $pconds;
 	}
 
-	/* set order */
-	public function set_order($col, $crit) {
-		$this->order = array($col => $crit);
+	public function auto_conds() {
+		/* retrieve conds */
+		$conds = $this->wf->get_var($this->dsrc->get_name().'_filter');
+		/* compute datasource conds */
+		$this->set_conds($conds);
 	}
 
-	/* set fields (columns) */
-	public function set_fields($fields, $callback = array()) {
-		$this->fields = $fields;
-		$this->fields_callback = $callback;
+	public function set_rows_per_page($nb) {
+		$this->rows_per_page = max(0, $nb);
 	}
 
-	/* gen filter list */
-	public function gen_filters() {
-		$struct  = $this->dsrc->get_struct();
-		$data    = $this->dsrc->get_rdata();
+	public function set_page_nb($nb) {
+		$this->page_nb = max(1, $nb);
+	}
 
+	public function auto_page_nb() {
+		/* retrieve page number */
+		$this->set_page_nb($this->wf->get_var($this->dsrc->get_name().'_page'));
+	}
+
+	public function set_cols($cols) {
+		$this->cols = $cols;
+	}
+
+	public function set_row_callback($row_callback) {
+		$this->row_callback = $row_callback;
+	}
+
+	public function set_filters($filters) {
+		$this->filters = $filters;
+	}
+
+	public function get_cols() {
+		return($this->cols);
+	}
+
+	public function get_rows_per_page() {
+		return($this->rows_per_page);
+	}
+
+	public function get_page_nb() {
+		return($this->page_nb);
+	}
+
+	public function get_filters() {
 		$filters = array();
+		$struct  = $this->dsrc->get_struct();
 
 		/* consider filters */
 		foreach($this->filters as $col => $conf) {
@@ -121,37 +144,21 @@ class core_dataset {
 				);
 
 				/* select filter */
-				if($conf['type'] == WF_CORE_DATASET_SELECT_FILTER) {
+				if($conf['type'] = WF_CORE_DATASET_SELECT) {
 					/* get uniq list values */
-					$options = array();
-					foreach($data as $row) {
-						$value = $row[$col];
+					$filter['options'] = array();
+					$options = $this->dsrc->get_options($col);
+					foreach($options as $option) {
+						$value  = $option[$col];
+						$pvalue = $value;
 
 						/* consider callback */
-						$pvalue = $value;
 						if($conf['callback']) {
 							$pvalue = call_user_func($conf['callback'], $value);
 						}
 
-						$options[$value] = $pvalue;
+						$filter['options'][$value] = $pvalue;
 					}
-					$filter['options'] = $options;
-				}
-				/* slide filter */
-				else if($conf['type'] == WF_CORE_DATASET_SLIDE_FILTER) {
-					/* get min and max values */
-					$min =  PHP_INT_MAX;
-					$max = -PHP_INT_MAX;
-					foreach($data as $row) {
-						if($row[$col] < $min) {
-							$min = $row[$col];
-						}
-						if($row[$col] > $max) {
-							$max = $row[$col];
-						}
-					}
-					$filter['min'] = $min;
-					$filter['max'] = $max;
 				}
 
 				$filters[$col] = $filter;
@@ -161,60 +168,52 @@ class core_dataset {
 		return($filters);
 	}
 
-	/* gen data list */
-	public function gen_data() {
-		/* retrieve data from datasource */
-		$struct   = $this->dsrc->get_struct();
-		$in_data  = $this->dsrc->get_data();
-		$out_data = array();
+	public function get_rows() {
+		/* retrieve rows from datasource considering all options */
+		$rows = array();
 
-		/* consider order */
-		if($this->order) {
-			uasort($in_data, array($this, 'callback_sort_data'));
+		/* apply auto conditions, page number and ordering */
+		$this->auto_conds();
+		$this->auto_page_nb();
+		$this->auto_order();
+
+		/* number of page should not exceed total num rows */
+		if($this->rows_per_page) {
+			$this->page_nb = min(
+				ceil($this->get_total_num_rows() / $this->rows_per_page),
+				$this->page_nb
+			);
 		}
 
-		/* consider fields */
-		foreach($in_data as $row) {
-			/* consider fields callback */
-			if($this->fields_callback) {
-				$row = call_user_func($this->fields_callback, $row);
+		$data = $this->dsrc->get_data(
+			$this->conds,
+			$this->order,
+			($this->page_nb - 1) * $this->rows_per_page,
+			$this->rows_per_page
+		);
+
+		foreach($data as $datum) {
+			$row = array();
+			foreach($this->cols as $col => $conf) {
+				$row[$col] = $datum[$col];
 			}
 
-			/* generate data */
-			$field = array();
-			foreach($this->fields as $col => $conf) {
-				$field[] = $row[$col];
+			/* consider row callback */
+			if($this->row_callback) {
+				$row = call_user_func($this->row_callback, $row, $datum);
 			}
-			$out_data[] = $field;
+			$rows[] = $row;
 		}
 
-		return($out_data);
+		return($rows);
 	}
 
-	/* get fields */
-	public function get_fields() {
-		return($this->fields);
+	public function get_name() {
+		return($this->dsrc->get_name());
 	}
 
-	/* get form filter */
-	public function get_form_filter() {
-		return($this->form_filter);
+	public function get_total_num_rows() {
+		return($this->dsrc->get_num_rows($this->conds));
 	}
 
-	/* data sorting callback */
-	public function callback_sort_data($a, $b) {
-		$key  = key($this->order);
-		$crit = current($this->order);
-
-		if($a[$key] == $b[$key]) {
-			return(0);
-		}
-
-		if($a[$key] < $b[$key]) {
-			return(($crit == WF_ASC) ? -1 : 1);
-		}
-		else {
-			return(($crit == WF_ASC) ? 1 : -1);
-		}
-	}
 }
