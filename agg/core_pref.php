3770e140969ac0715ae4fda0ed5ac85b7e54531e
mykiimike <mv@verman.fr>
2011-06-14 11:55:14 +0000

    Little modification in core pref

--- agg/core_pref.php
+++ agg/core_pref.php
@@ -1,31 +1,10 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Web Framework 1                                       *
- * BinarySEC (c) (2000-2008) / www.binarysec.com         *
- * Author: Michael Vergoz <mv@binarysec.com>             *
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~         *
- *  Avertissement : ce logiciel est protégé par la       *
- *  loi du copyright et par les traités internationaux.  *
- *  Toute personne ne respectant pas ces dispositions    *
- *  se rendra coupable du délit de contrefaçon et sera   *
- *  passible des sanctions pénales prévues par la loi.   *
- *  Il est notamment strictement interdit de décompiler, *
- *  désassembler ce logiciel ou de procèder à des        *
- *  opération de "reverse engineering".                  *
- *                                                       *
- *  Warning : this software product is protected by      *
- *  copyright law and international copyright treaties   *
- *  as well as other intellectual property laws and      *
- *  treaties. Is is strictly forbidden to reverse        *
- *  engineer, decompile or disassemble this software     *
- *  product.                                             *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 define("CORE_PREF_NUM",      900);
 define("CORE_PREF_BOOL",     902);
 define("CORE_PREF_VARCHAR",  903);
 define("CORE_PREF_DATA",     904);
-define("CORE_PREF_SELECT",   905);
+// define("CORE_PREF_SELECT",   905);
 
 class core_pref_context {
 	public $wf;
