c4de6b97109f9692e90d9bc134f6cf55428ea022
mykiimike <mv@verman.fr>
2011-07-11 06:42:24 +0000

    Release 1.2.1

--- module.php
+++ module.php
@@ -8,8 +8,8 @@ class wfm_core extends wf_module {
 	/* mimetype icon (c) FAMFAMFAM */
 	public function get_name() { return("core"); }
 	public function get_description()  { return("OWF Native Core module"); }
-	public function get_banner()  { return("OWF Native Core/1.2.1"); }
-	public function get_version() { return("1.2.1"); }
+	public function get_banner()  { return("OWF Native Core/1.2.2"); }
+	public function get_version() { return("1.2.2"); }
 	public function get_authors() { return("Michael VERGOZ"); }
 	public function get_depends() { return(NULL); }
 	
