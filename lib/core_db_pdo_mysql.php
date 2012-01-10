60982d8c15deca68e0bc62dc8d07920a1aa40833
mykiimike <mv@verman.fr>
2012-01-10 05:36:42 +0000

    Adding !== for not egal comparaison

--- lib/core_db_pdo_mysql.php
+++ lib/core_db_pdo_mysql.php
@@ -940,7 +940,10 @@ class core_db_pdo_mysql extends core_db {
 				array_push($prepare_values, $val);
 				break;
 			case '!=':
-				$cond = "$var != ?";
+				$cond = "$var != $val";
+				break;
+			case '!==':
+				$cond = "$var <> ?";
 				array_push($prepare_values,$val);
 				break;
 			/* added by keo on 11/12/2008 : IS NULL and IS NOT NULL conditions */
