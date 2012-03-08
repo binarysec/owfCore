31b6bc2778aeb281bf0b0bccc4237b52f685f261
mykiimike <mv@verman.fr>
2012-03-08 10:09:49 +0000

    Adding core lock support by file
    git-svn-id: svn+ssh://svn.owf.re/home/svn/nat/core/branches/1_2@357 f8094afe-d156-4993-a692-4f49031433ae

---
+++ agg/core_lock.php
@@ -0,0 +1,81 @@
+<?php
+
+class core_lock extends wf_agg {
+
+	public function loader($wf) {
+		$this->wf = &$wf;
+		
+		$this->lock_dir = $this->wf->get_last_filename("core/locks");
+		$this->wf->create_dir($this->lock_dir."/pad");
+		
+	}
+	
+	public function try_lock($group, $resource, $type) {
+	
+		$lf = $this->lock_dir."/$group-$resource.lock";
+		
+		$fp = fopen($lf, 'a+');
+		if(!$fp) {
+			echo "Can not open $lf\n";
+			return(false);
+		}
+		
+		if(flock($fp, LOCK_EX)) {
+			$data = fgets($fp);
+			$serial = @unserialize($data);
+			if(!is_array($serial)) {
+				$serial = array(
+					"group" => $group,
+					"resource" => $resource,
+					"type" => $type,
+					"lock" => false,
+					"pid" => -1,
+				);
+			}
+			
+			/* site is locked */ 
+			if($serial["lock"] == true) {
+				/* control the pid if it is dead */
+				$pf = "/proc/$serial[pid]";
+				if(!file_exists($pf)) {
+					echo "*\n".
+						"* THE RESOURCE $serial[resource] UNDER GROUP $serial[group]\n".
+						"* IS ACTUALY LOCKED BY A DEAD PROCESS n°$serial[pid]\n".
+						"* YOU MUST REMOVE MANUALY THE LOCKS\n".
+						"* File to remove: $lf\n";
+				}
+
+				flock($fp, LOCK_UN);
+				fclose($fp);
+				return(false);
+			}
+			
+			/* lock the file */
+			$serial["lock"] = true;
+			$serial["pid"] = getmypid();
+				
+			/* write the content */
+			ftruncate($fp, 0);
+			fputs($fp, serialize($serial));
+			flock($fp, LOCK_UN);
+		}
+		else {
+			fclose($fp);
+			return(false);
+		}
+		
+		fclose($fp); 
+
+		return(true);
+		
+	}
+
+	public function unlock($group, $resource) {
+		$lf = $this->lock_dir."/$group-$resource.lock";
+		if(file_exists($lf))
+			unlink($lf);
+	}
+	
+	
+}
+
