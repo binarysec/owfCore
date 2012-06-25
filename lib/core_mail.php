d0f3c0d2b78da33f840da1866dc5105c1e14d787
mykiimike <mv@verman.fr>
2012-06-25 07:03:25 +0000

    Some addition

--- lib/core_mail.php
+++ lib/core_mail.php
@@ -7,26 +7,28 @@ class core_mail {
 	
 	private $headers = array();
 	private $body;
+	private $template = "var/tpl/core/mail/base";
 	
-	public function __construct($wf) {
+	public function __construct($wf, $mail_from, $rcpt_to, $subject, $content) {
 		$this->wf = $wf;
 		$this->core_smtp = $this->wf->core_smtp();
 		$this->core_lang = $this->wf->core_lang();
-	}
-	
-	public function set_body($body) {
-		$this->body = $body;
 	}
 	
 	public function add_header($header_name, $data) {
 		$this->headers[$header_name] = $data;
 	}
 	
-	public function set_headers(array $headers) {
-		$this->headers = $headers;
+	public function attach($file) {
+
 	}
-	
-	public function merge_headers(array $headers) {
-		$this->headers = array_merge($this->headers, $headers);
+
+	public function render($file=null) {
+
 	}
+
+	public function send() {
+
+	}
+
 }
