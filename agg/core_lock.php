58007172602a94a5cfb96731b0a16ea42b9caf47
Dagrut <mfl@verman.fr>
2012-09-09 07:43:19 +0000

    Fixed a mistake in core_lock : Was unlocking but returning true when the old locking process was dead.

--- agg/core_lock.php
+++ agg/core_lock.php
@@ -34,21 +34,19 @@ class core_lock extends wf_agg {
 			
 			/* site is locked */ 
 			if($serial["lock"] == true) {
-				$ret = false;
 				/* control the pid if it is dead */
 				$pf = "/proc/$serial[pid]";
 				if(!file_exists($pf)) {
 					echo "*\n".
 						"* THE RESOURCE $serial[resource] UNDER GROUP $serial[group]\n".
 						"* IS ACTUALY LOCKED BY A DEAD PROCESS n°$serial[pid]\n".
-						"* Removing file $lf\n";
-					@unlink($lf);
-					$ret = true;
+						"* Recreating lock\n";
 				}
-				
-				flock($fp, LOCK_UN);
-				fclose($fp);
-				return($ret);
+				else {
+					flock($fp, LOCK_UN);
+					fclose($fp);
+					return($ret);
+				}
 			}
 			
 			/* lock the file */
@@ -76,7 +74,5 @@ class core_lock extends wf_agg {
 		if(file_exists($lf))
 			unlink($lf);
 	}
-	
-	
 }
 
