88cdd10a885fb55d8c550afea838ab9c5c4223d6
olivier <ol@verman.fr>
2014-07-24 12:19:01 +0400

    In bin/mail_spooler : - Added -h or --help to show full options - Added --block to stop retries for mails after a delay in seconds - Added --clean to remove sent mail entries from the database after a delay - Added -f combining with --clean to also remove unsent mails - Sanatizing count, timeout, block and clean values

--- bin/mail_spooler.php
+++ bin/mail_spooler.php
@@ -6,8 +6,16 @@ if(!defined("OWFCONSOLE"))
 class core_mail_spooler extends wf_cli_command {
 	
 	public function help() {
-		$this->wf->msg("Usage: core mail_spooler [--count <x>] [--timeout <x>] [-r] [--retry] [-v] [--verbose]");
-		$this->wf->msg("Sample: core mail_spooler --count 50 -r -v");
+		$this->wf->msg("core mail_spooler [--count <x>] [--timeout <x>] [-r --retry] [-v --verbose] [--block <x>] [--clean <x> [-f]] [-h --help]");
+		$this->wf->msg("");
+		$this->wf->msg("--count <x>    : Send only the first x mails");
+		$this->wf->msg("--timeout <x>  : Wait x seconds trying to connect to the mail server");
+		$this->wf->msg("-r / --retry   : Retry mails failed to be sent");
+		$this->wf->msg("-v / --verbose : Output status messages");
+		$this->wf->msg("--block <x>    : Block retry for mails failed to be sent for x seconds");
+		$this->wf->msg("--clean <x>    : Remove sent mails older than x seconds from database");
+		$this->wf->msg("--clean <x> -f : Also remove unsent mails");
+		$this->wf->msg("-h / --help    : Show this message");
 	}
 	
 	public function process() {
@@ -24,13 +32,25 @@ class core_mail_spooler extends wf_cli_command {
 			return false;
 		}
 		
+		if($this->wf->get_opt("h") || $this->wf->get_opt("help"))
+			return $this->help();
+		
 		/* options */
 		$verbose = $this->wf->get_opt("v") || $this->wf->get_opt("verbose");
 		$retry = $this->wf->get_opt("r") || $this->wf->get_opt("retry");
 		$count = intval($this->wf->get_opt("count", true));
-		$timeout = intval($this->wf->get_opt("timeout", true));
+		$timeout = $this->wf->get_opt("timeout", true);
+		$block = $this->wf->get_opt("block", true);
+		$this->clean = $this->wf->get_opt("clean", true);
+		$this->force = $this->wf->get_opt("f") || $this->wf->get_opt("force");
 		if(!$count)
 			$count = $core_mail_spool->dao->count(array(array("queue", ($retry ? "<=" : "="), 0)));
+		
+		/* sanatize */
+		$count = is_numeric($count) ? intval($count) : false;
+		$timeout = is_numeric($timeout) ? intval($timeout) : false;
+		$block = is_numeric($block) ? intval($block) : false;
+		$this->clean = is_numeric($this->clean) ? intval($this->clean) : false;
 		
 		if($count) {
 			if($verbose)
@@ -62,10 +82,17 @@ class core_mail_spooler extends wf_cli_command {
 					$sent = is_string($queue) && $queue > 0;
 					
 					/* update database */
-					$core_mail_spool->dao->modify(
-						array("id" => $mail["id"]),
-						array("queue" => $queue, "send_time" => $sent ? time() : -1)
-					);
+					if(!$sent && $block && $mail["create_time"] < (time() - $block)) {
+						$core_mail_spool->dao->modify(
+							array("id" => $mail["id"]),
+							array("queue" => "FAILED", "send_time" => time())
+						);
+					}
+					else
+						$core_mail_spool->dao->modify(
+							array("id" => $mail["id"]),
+							array("queue" => $queue, "send_time" => $sent ? time() : -1)
+						);
 				}
 			}
 			
@@ -80,9 +107,27 @@ class core_mail_spooler extends wf_cli_command {
 				$this->wf->msg("No mails to send.");
 		}
 		
+		$this->clean();
+		
 		$core_lock->unlock("core", "mail_spooler");
 		
 		return true;
+	}
+	
+	private function clean() {
+		if(!$this->clean)
+			return false;
+		
+		$q = new core_db_adv_delete("core_mail_spool");
+		$q->do_comp("create_time", "<", time() - $this->clean);
+		if(!$this->force) {
+			$q->do_comp("queue", "!=", 0);
+			$q->do_comp("queue", "!=", -1);
+			$q->do_comp("queue", "!=", "FAILED");
+		}
+		$ret = $this->wf->db->query($q);
+		
+		return $ret;
 	}
 	
 }
