f8a57290e371cf363b9d9dddaea2a9d37381c688
olivier <ol@verman.fr>
2014-07-24 12:37:29 +0400

    Removed -f from clean option and added --fclean <x> : it removes all unsent mails after a delay of x seconds

--- bin/mail_spooler.php
+++ bin/mail_spooler.php
@@ -6,7 +6,7 @@ if(!defined("OWFCONSOLE"))
 class core_mail_spooler extends wf_cli_command {
 	
 	public function help() {
-		$this->wf->msg("core mail_spooler [--count <x>] [--timeout <x>] [-r --retry] [-v --verbose] [--block <x>] [--clean <x> [-f]] [-h --help]");
+		$this->wf->msg("core mail_spooler [--count <x>] [--timeout <x>] [-r --retry] [-v --verbose] [--block <x>] [--clean <x>] [--fclean <x>] [-h --help]");
 		$this->wf->msg("");
 		$this->wf->msg("--count <x>    : Send only the first x mails");
 		$this->wf->msg("--timeout <x>  : Wait x seconds trying to connect to the mail server");
@@ -14,7 +14,7 @@ class core_mail_spooler extends wf_cli_command {
 		$this->wf->msg("-v / --verbose : Output status messages");
 		$this->wf->msg("--block <x>    : Block retry for mails failed to be sent for x seconds");
 		$this->wf->msg("--clean <x>    : Remove sent mails older than x seconds from database");
-		$this->wf->msg("--clean <x> -f : Also remove unsent mails");
+		$this->wf->msg("--fclean <x>   : Remove unsent mails older than x seconds from database");
 		$this->wf->msg("-h / --help    : Show this message");
 	}
 	
@@ -42,7 +42,7 @@ class core_mail_spooler extends wf_cli_command {
 		$timeout = $this->wf->get_opt("timeout", true);
 		$block = $this->wf->get_opt("block", true);
 		$this->clean = $this->wf->get_opt("clean", true);
-		$this->force = $this->wf->get_opt("f") || $this->wf->get_opt("force");
+		$this->fclean = $this->wf->get_opt("fclean", true);
 		if(!$count)
 			$count = $core_mail_spool->dao->count(array(array("queue", ($retry ? "<=" : "="), 0)));
 		
@@ -51,6 +51,7 @@ class core_mail_spooler extends wf_cli_command {
 		$timeout = is_numeric($timeout) ? intval($timeout) : false;
 		$block = is_numeric($block) ? intval($block) : false;
 		$this->clean = is_numeric($this->clean) ? intval($this->clean) : false;
+		$this->fclean = is_numeric($this->fclean) ? intval($this->fclean) : false;
 		
 		if($count) {
 			if($verbose)
@@ -115,15 +116,31 @@ class core_mail_spooler extends wf_cli_command {
 	}
 	
 	private function clean() {
-		if(!$this->clean)
+		if(!$this->clean && !$this->fclean)
 			return false;
 		
 		$q = new core_db_adv_delete("core_mail_spool");
-		$q->do_comp("create_time", "<", time() - $this->clean);
-		if(!$this->force) {
+		if($this->clean) {
+			$q->do_open();
+			$q->do_comp("create_time", "<", time() - $this->clean);
 			$q->do_comp("queue", "!=", 0);
 			$q->do_comp("queue", "!=", -1);
 			$q->do_comp("queue", "!=", "FAILED");
+			$q->do_close();
+		}
+		if($this->fclean) {
+			if($this->clean)
+				$q->do_or();
+			$q->do_open();
+			$q->do_comp("create_time", "<", time() - $this->fclean);
+			$q->do_open();
+				$q->do_comp("queue", "=", 0);
+				$q->do_or();
+				$q->do_comp("queue", "=", -1);
+				$q->do_or();
+				$q->do_comp("queue", "=", "FAILED");
+			$q->do_close();
+			$q->do_close();
 		}
 		$ret = $this->wf->db->query($q);
 		
