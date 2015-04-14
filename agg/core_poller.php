a3e1c69d6b5064f4e9d20f0e37bd81052e906ad1
Olivier Leclercq <ol@verman.fr>
2015-04-14 16:22:54 +0400

    Now able to specify the type of the polled event : json, callback or raw Now processing callback events before sending the results Added function to format data for the database regarding the event type

--- agg/core_poller.php
+++ agg/core_poller.php
@@ -1,5 +1,9 @@
 <?php
 
+define("CORE_POLLER_JSON",		0);
+define("CORE_POLLER_CALLBACK",	1);
+define("CORE_POLLER_RAW",		2);
+
 class core_poller extends wf_agg {
 	
 	public function loader() {
@@ -28,6 +32,17 @@ class core_poller extends wf_agg {
 					"perm" => array("session:admin"),
 					"name" => "Data",
 				),
+				"type" => array(
+					"type" => WF_SMALLINT | WF_UNSIGNED,
+					"perm" => array("session:admin"),
+					"name" => "Type",
+					"kind" => OWF_DAO_SELECT,
+					"list" => array(
+						CORE_POLLER_JSON		=> "JSON data",
+						CORE_POLLER_CALLBACK	=> "Callback",
+						CORE_POLLER_RAW			=> "Raw data",
+					)
+				),
 			),
 		);
 		
@@ -41,6 +56,10 @@ class core_poller extends wf_agg {
 		);
 	}
 	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 *
+	 * The poll call done by the client to retrieve events
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 	public function poll() {
 		$short = $this->wf->get_var("owfcp_short") || $this->wf->get_var("owfcps");
 		$w = intval($this->wf->get_var("wait"));
@@ -50,16 +69,19 @@ class core_poller extends wf_agg {
 		$uid = $this->session->session_me["id"];
 		$events = array();
 		
+		/* short polling : fetch events */
 		if($short) {
 			$polled_events = $this->dao->get(array("user_id" => $uid));
 			foreach($polled_events as $v)
-				$events[] = unserialize($v["data"]);
+				$events[] = $v;
 		}
+		
+		/* long polling : loop until events or timeout */
 		else {
 			do {
 				$polled_events = $this->dao->get(array("user_id" => $uid));
 				foreach($polled_events as $v)
-					$events[] = unserialize($v["data"]);
+					$events[] = $v;
 				
 				$timeout--;
 				if($timeout)
@@ -67,8 +89,40 @@ class core_poller extends wf_agg {
 			} while(empty($events) && $timeout);
 		}
 		
-		foreach($polled_events as $v)
+		foreach($events as $k => $v) {
+			
+			/* remove event from database */
 			$this->dao->remove(array("id" => $v["id"]));
+			
+			/* process event */
+			switch($v["type"]) {
+				case CORE_POLLER_RAW :
+					$events[$k] = $v["data"];
+					break;
+				case CORE_POLLER_CALLBACK :
+					$cb_info = unserialize($v["data"]);
+					if(is_array($cb_info)) {
+						$class = array_shift($cb_info);
+						$method = array_shift($cb_info);
+						array_unshift($cb_info, $this->wf);
+						$ret = call_user_func_array(array($class, $method), $cb_info);
+					}
+					elseif(is_string($cb_info)) {
+						$ret = $cb_info($this->wf);
+					}
+					
+					if($ret !== false) {
+						$events[$k] = $ret;
+					}
+					
+					break;
+				case CORE_POLLER_JSON :
+				default :
+					$events[$k] = unserialize($v["data"]);
+					break;
+			}
+			
+		}
 		
 		$moar = $this->wf->execute_hook("owf_core_poller");
 		
@@ -79,13 +133,44 @@ class core_poller extends wf_agg {
 		exit(0);
 	}
 	
-	public function add($json, $uid = 0) {
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 *
+	 * Add an event to the polling system which will be delivered to the end client
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+	public function add($data, $uid = 0, $type = CORE_POLLER_JSON) {
 		if(!$uid)
 			$uid = $this->session->session_me["id"];
 		
-		$this->dao->add(array(
-			"user_id" => $uid,
-			"data" => serialize($json)
-		));
+		$data = $this->format_data($data, $type);
+		
+		if($data !== false) {
+			$this->dao->add(array(
+				"user_id"	=> $uid,
+				"data"		=> $data,
+				"type"		=> $type
+			));
+		}
+	}
+	
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 *
+	 * Format data before database insertion
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+	public function format_data($data, $type) {
+		
+		switch($type) {
+			case CORE_POLLER_RAW :
+				return $data;
+			case CORE_POLLER_CALLBACK :
+				if(	(is_string($data) && !function_exists($data)) ||
+					(is_array($data) && !method_exists($data[0], $data[1]))
+					) {
+						return false;
+				}
+			case CORE_POLLER_JSON :
+			default :
+				return serialize($data);
+		}
+		
 	}
 }
